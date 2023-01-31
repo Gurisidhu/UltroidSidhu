9c855cc005abd92419928f52cd6ea5bc47248bd6
Gurisidhu <112066529+Gurisidhu@users.noreply.github.com>
2023-01-31 17:31:17 +0530

    Update configs.py

--- pyUltroid/configs.py
+++ pyUltroid/configs.py
@@ -20,12 +20,12 @@ except ImportError:
 class Var:
     # mandatory
     API_ID = (
-        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=6, cast=int)
+        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=18734981, cast=int)
     )
     API_HASH = (
         sys.argv[2]
         if len(sys.argv) > 2
-        else config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+        else config("API_HASH", default="0e88a144af021c41897a8e826eaf96aa")
     )
     SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
     REDIS_URI = (
