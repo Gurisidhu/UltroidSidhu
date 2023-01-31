05aa205c558194bcaa57a5f7931458bd04172f8f
Gurisidhu <112066529+Gurisidhu@users.noreply.github.com>
2023-01-31 17:33:58 +0530

    Update configs.py

--- pyUltroid/configs.py
+++ pyUltroid/configs.py
@@ -27,14 +27,14 @@ class Var:
         if len(sys.argv) > 2
         else config("API_HASH", default="0e88a144af021c41897a8e826eaf96aa")
     )
-    SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
+    SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=1BVtsOL8Bu5pSWCpFVEpOtj0sb7QnoLYgoKFR6ZPvNZ8OjEVkjyctQ5nO3g4gubtOOnjAun1WG2PU15ieh7aRaSo1nn97MEeQcZFpkm-8h_6lFGE_2n8hI7QMNVRojwbSZTHoTaYxxuj9cBcu4hPSNG8JRA986bgdtfEOGJZU0tncUj4XKecpRWX20LDpMSqJubzLdUqUuoFlLXL8nu3a60VQ8xUz4F3eTd5loEG0yXgmOlYa_64q9MGnrlodMhZOol7LzPg_nF6WComDdijwvA2Zop-OUI8pQaPTr4UGOT2gSZeaaqRdsZNsy11VhFPbiow-m33spS3msc_cTsITeM7XT8epvzg=)
     REDIS_URI = (
         sys.argv[4]
         if len(sys.argv) > 4
-        else (config("REDIS_URI", default=None) or config("REDIS_URL", default=None))
+        else (config("REDIS_URI", default=redis-18203.c241.us-east-1-4.ec2.cloud.redislabs.com:18203) or config("REDIS_URL", default=None))
     )
     REDIS_PASSWORD = (
-        sys.argv[5] if len(sys.argv) > 5 else config("REDIS_PASSWORD", default=None)
+        sys.argv[5] if len(sys.argv) > 5 else config("REDIS_PASSWORD", default=GzqsjNTpugv9DZvKchuQSwrlKevLG4yE)
     )
     # extras
     BOT_TOKEN = config("BOT_TOKEN", default=None)
